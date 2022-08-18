4638901076f5e28ff0a5c3d9fd916c5e0c2a93a1
TranHauTruong <kandytv1@gmail.com>
2022-08-18 19:35:31 +0700

    done Project

---
+++ index.js
@@ -0,0 +1,36 @@
+function random(array) {
+    return array[Math.floor(Math.random() * array.length)]
+}
+
+function comparison(key) {
+    switch (key) {
+        case 'ks':
+        case 'bk':
+        case 'sb':
+            return 'win';
+        case 'bs':
+        case 'kb':
+        case 'sk':
+            return 'lose';
+        case 'kk':
+        case 'ss':
+        case 'bb':
+            return '==';
+    }
+}
+
+function sum(elemenOne, elemenTwo) {
+    return elemenOne + elemenTwo;
+}
+
+function gameplay(options) {
+    const { typeGames, playerOne } = options;
+    const playerTwo = random(typeGames);
+    const sumTypes = sum(playerOne, playerTwo);
+    return comparison(sumTypes);
+}
+
+
+const typeGames = ['k', 'b', 's'];
+let playerOne = 'b';
+console.log(gameplay({ typeGames, playerOne }));
